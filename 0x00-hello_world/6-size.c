59237625a2333356dde8f76eb283ad53d3c14fb9
Amr Ghanem <amrghanem467@gmail.com>
2023-03-20 15:29:02 -0700

    6 check 1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)", (unsigned long)sizeof(h));
-	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(g));
+	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(j));
 	printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(f));
-	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(s);
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(s));
 	return (0);
 }
