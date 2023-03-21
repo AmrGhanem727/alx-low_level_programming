84b5ffb5b3d12b7bf0d8ddcadd7085ccc81da600
Amr Ghanem <amrghanem467@gmail.com>
2023-03-20 17:06:46 -0700

    check

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\"- Dora Korpar. 2015-10-19\n";
+	fputs("and that piece of art is useful\"- Dora Korpar. 2015-10-19\n", stderr);
 
 	write(1, quote, 59);
 	return (1);
