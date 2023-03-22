1b77d7b930809f44eebce4a0cf12c11e50b2b3e5
Amr Ghanem <amrghanem467@gmail.com>
2023-03-22 16:08:29 -0700

    check

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
+#include <main.h>
 /**
- * main - entry point
- *
- * Return: 0
+ * main - It's the entry point for the program
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("_puthcar\n");
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
