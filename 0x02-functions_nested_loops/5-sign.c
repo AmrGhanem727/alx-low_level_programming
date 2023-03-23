1e1feb85175552a280bba67cdd045f4a3f85288d
Amr Ghanem <amrghanem467@gmail.com>
2023-03-23 11:52:40 -0700

    check

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,20 +2,25 @@
 /**
  * print_sign - Checks the sign of the entered
  * @n: The checked character
- * Return: 1 if positive 0 if zero -1 if negative
+ * Return: 1 and prints + if positive 
+ * 0 if zero 
+ * -1 and prints - if negative
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
+		_putchar(48);
 		return (0);
 	}
 	else
 	{
+		_putchar('-');
 		return (-1);
 	}
 }
