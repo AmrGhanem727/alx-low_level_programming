650f1a149f00c3cced721665e83eff33d6a95ea1
Amr Ghanem <amrghanem467@gmail.com>
2023-03-23 10:16:25 -0700

    sign checked

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - Checks the sign of the entered
+ * @n: The checked character
+ * Return: 1 if positive 0 if zero -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
