2f73d8615fc7e94623731c5ca104e1b48d01182b
Amr Ghanem <amrghanem467@gmail.com>
2023-03-24 05:02:26 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers x10 times from 0 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while (j <= 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
