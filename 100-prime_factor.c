368be9703c5356bcd31607a3a4d7b2f2762fa163
Amr Ghanem <amrghanem467@gmail.com>
2023-03-24 18:50:13 -0700

    prime

---
+++ 100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -prints the largest prime factor
+ * Return: always 0
+ */
+int main(void)
+{
+	long x, maxh;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxh = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
